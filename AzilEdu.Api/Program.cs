ceb01780c90c6e3762906aea03325df74f79659d
maritapodrug <marita.podrug211@gmail.com>
2026-06-18 19:45:45 +0200

    Predavanje 4.

--- AzilEdu.Api/Program.cs
+++ AzilEdu.Api/Program.cs
@@ -1,12 +1,33 @@
+using AzilEdu.Api.Data;
+using Microsoft.EntityFrameworkCore;
+using AzilEdu.Shared.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
+
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
 builder.Services.AddOpenApi();
 
+
+builder.Services.AddDbContext<AzilEduDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -19,5 +40,92 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AzilEduDbContext>();
+
+    await db.Database.MigrateAsync();
+
+    if (!await db.Animals.AnyAsync())
+    {
+        db.Animals.AddRange(
+            new Animal
+            {
+                Name = "Luna",
+                Species = "Pas",
+                Breed = "Labrador",
+                Gender = "Ženka",
+                Age = 3,
+                ArrivalDate = new DateTime(2025, 10, 12),
+                IsAdopted = false,
+                ImageUrl = "/images/animals/luna.webp",
+                Description = "Mirna i druželjubiva kujica koja voli šetnje."
+            },
+            new Animal
+            {
+                Name = "Maza",
+                Species = "Mačka",
+                Breed = "Domaća kratkodlaka",
+                Gender = "Ženka",
+                Age = 2,
+                ArrivalDate = new DateTime(2025, 11, 5),
+                IsAdopted = true,
+                ImageUrl = "/images/animals/maza.webp",
+                Description = "Zaigrana mačka naviknuta na boravak u zatvorenom prostoru."
+            },
+            new Animal
+            {
+                Name = "Rex",
+                Species = "Pas",
+                Breed = "Njemački ovčar",
+                Gender = "Mužjak",
+                Age = 5,
+                ArrivalDate = new DateTime(2026, 1, 20),
+                IsAdopted = false,
+                ImageUrl = "/images/animals/rex.webp",
+                Description = "Aktivan pas koji traži iskusnijeg vlasnika."
+            },
+            new Animal
+            {
+                Name = "Nala",
+                Species = "Mačka",
+                Breed = "Maine Coon mješanac",
+                Gender = "Ženka",
+                Age = null,
+                ArrivalDate = new DateTime(2026, 2, 3),
+                IsAdopted = false,
+                ImageUrl = "/images/animals/nala.webp",
+                Description = "Mlada mačka pronađena bez poznate povijesti."
+            },
+            new Animal
+            {
+                Name = "Tobi",
+                Species = "Pas",
+                Breed = "Mješanac",
+                Gender = "Mužjak",
+                Age = 1,
+                ArrivalDate = null,
+                IsAdopted = false,
+                ImageUrl = "/images/animals/tobi.webp",
+                Description = "Vesel pas kojem datum dolaska još nije potvrđen."
+            },
+            new Animal
+            {
+                Name = "Bruno",
+                Species = "Pas",
+                Breed = "Bigl",
+                Gender = "Mužjak",
+                Age = 4,
+                ArrivalDate = new DateTime(2025, 9, 18),
+                IsAdopted = true,
+                ImageUrl = "/images/animals/bruno.webp",
+                Description = "Udomljen pas koji ostaje u evidenciji azila."
+            }
+        );
+
+        await db.SaveChangesAsync();
+    }
+}
 
 app.Run();
